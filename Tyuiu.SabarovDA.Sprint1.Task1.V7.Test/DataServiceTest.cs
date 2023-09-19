16d68807a9c022a83b3f215062c45667d573d545
Tyuiu.SabarovDA@outlook.com <Tyuiu.SabarovDA@outlook.com>
2023-09-19 23:45:21 +0300

    Task1

---
+++ Tyuiu.SabarovDA.Sprint1.Task1.V7.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.SabarovDA.Sprint1.Task1.V7.Lib;
+
+namespace Tyuiu.SabarovDA.Sprint1.Task1.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 3.0;
+            double y = 2.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(3, res);
+
+        }
+    }
+}
